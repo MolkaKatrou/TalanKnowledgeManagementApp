d7ea712ff147e9ee9fd73770f6d1f60a1c032197
MolkaKatrou <102295876+MolkaKatrou@users.noreply.github.com>
2022-04-04 11:50:41 +0100

    auth + forgrt/reset password + send email (sprint 1 END)

--- backend/controllers/users.controlers.js
+++ backend/controllers/users.controlers.js
@@ -1,10 +1,23 @@
 const UserModel = require("../models/users.model");
 const ValidateUser = require("../Validation/users.validation");
 const ValidateLogin = require("../Validation/Login.validation");
+const ValidateReset = require("../Validation/reset.validation");
+
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require('crypto')
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+const usersModel = require("../models/users.model");
 
-const AddUser = async (req, res) => {
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth:{
+    api_key: "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"
+  }
+  }))
+
+  const AddUser = async (req, res) => {
   const { errors, isValid } = ValidateUser(req.body);
   try {
     if (!isValid) {
@@ -17,8 +30,23 @@ const AddUser = async (req, res) => {
           res.status(404).json(errors);
         } else {
           const hash = bcrypt.hashSync(req.body.password, 10); //hashed password
+          password = req.body.password
           req.body.password = hash;
           await UserModel.create(req.body);
+          transporter.sendMail({
+            to: req.body.email,
+            from:"molka.katrou@ensi-uma.tn",
+            subject:"User Identification",
+            html:  `<p>Hello </strong><span style="text-transform:uppercase">${req.body.firstname}</span><strong></strong>, <p/>
+            <p>Welcome to to our knowledge management application at TALAN <br/>
+            <ul>
+            <li>Your Email is : \n<b>${req.body.email} </li> <br/>
+            <li>Your password is : \n<b>${password}</li> 
+            </ul>
+            Best regards.
+            </p>` 
+          })
+         
           res.status(201).json({ message: "User added with success!" });
         }
       });
@@ -52,14 +80,12 @@ const UpdateUser = async (req, res) => {
     if (!isValid) {
       res.status(404).json(errors);
     } else {
-      await UserModel.findOne({ email: req.user.email }).then(async (exist) => {
-        const data = await UserModel.findOneAndUpdate(
-          { _id: req.params.id },
-          req.body,
-          { new: true }
-        );
-        res.status(201).json(data);
-      });
+      const data = await UserModel.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        { new: true }
+      );
+      res.status(201).json(data);
     }
   } catch (error) {
     console.log(error.message);
@@ -95,11 +121,16 @@ const Login = async(req, res)=>{
           }else{
             var token = jwt.sign({ 
               id: user._id,
-              name: user.name,
+              username: user.username,
               email: user.email,
-              role: user.role
+              role: user.role,
+              occupation: user.occupation,
+              lastname: user.lastname,
+              firstname: user.firstname,
+              phone: user.phone
              }, 
-               process.env.PRIVATE_KEY,  { expiresIn: '1h' });
+               process.env.PRIVATE_KEY,  { expiresIn: '10d' });
+               const {_id,username,email,role,occupation,lastname,firstname,phone} = user
                res.status(200).json({
                message: "success",
                token: "Bearer "+ token 
@@ -114,6 +145,63 @@ const Login = async(req, res)=>{
   res.status(404).json(error.message);
  }
 }
+
+
+
+const Resetpassword = async (req, res) => {
+  crypto.randomBytes(32, (err, buffer)=>{
+    if (err){
+      console.log(err)
+    }
+    const token = buffer.toString("hex")
+    usersModel.findOne({email:req.body.email})
+    .then(user =>{
+      if(!user){
+        return res.status(422).json({error: "user don't exist "})
+      }
+
+      user.resetToken = token
+      user.expireToken = Date.now() + 36000
+      user.save().then((result) => {
+        transporter.sendMail({
+          to: user.email,
+          from :"molka.katrou@ensi-uma.tn",
+          subject: "Password Reset",
+          html: `<p>Your requested for password reset</p>
+          <h5>Click in this link <a href="http://localhost:3000/resetpassword/${token}">Link<a/>to reset your password</h5>
+          ` 
+
+        })
+        res.json({message:"check your email"})
+      })
+
+    })
+  }
+)
+
+}
+const Newpassword = (req, res) => {
+  const newPassword = req.body.password
+  const sentToken = req.body.token
+  const { errors, isValid } = ValidateReset(req.body);
+  UserModel.findOne({resetToken:sentToken})
+  .then(user=>{
+      if(!user){
+          return res.status(422).json({error:"Try again session expired"})
+      }
+      bcrypt.hash(req.body.password,12).then(hashedpassword=>{
+         req.body.password = hashedpassword
+         user.resetToken = undefined
+         user.expireToken = undefined
+         user.save().then((saveduser)=>{
+             res.json({message:"password updated success"})
+         })
+      })
+  }).catch(err=>{
+      console.log(err)
+  })
+}
+
 const Test = (req, res) => {
   res.send("user")
 };
@@ -131,4 +219,6 @@ module.exports = {
   FindSingleUser,
   UpdateUser,
   DeleteUser,
+  Resetpassword,
+  Newpassword
 };
