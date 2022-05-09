2d7e61ed3e6f7fa9306a5b15f5de48b61e4941ba
MolkaKatrou <102295876+MolkaKatrou@users.noreply.github.com>
2022-05-09 11:26:47 +0100

    Sprint 2 (End) + Docker + start sprint 1 (Q/A)

---
+++ frontend/src/pages/home/Bookmark.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import { Container, makeStyles,CircularProgress } from "@material-ui/core"
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Post from '../../Components/posts/Post'
+import { getAllPosts } from '../../Redux/Actions/postsActions';
+import { useDispatch, useSelector } from 'react-redux';
+import Home from './Home';
+import { Grid } from '@mui/material';
+import BookmarkIcon from '@mui/icons-material/BookmarkOutlined';
+import { HomeContext } from '../../Context/HomeContext';
+
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        paddingTop: theme.spacing(10),
+        height:'100%',
+        backgroundColor:'rgb(225, 228, 232)'
+    },
+    mainContainer: {
+        borderRadius: 15,
+        paddingTop: theme.spacing(5),
+        margin: '10px 0',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px 50px',
+      },
+    loadingPaper: {
+        display: 'flex',
+        justifyContent: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop:'250px'
+      },
+}));
+
+function Bookmark() {
+    const show=true
+    const classes = useStyles()
+    const dispatch = useDispatch()
+    const auth = useSelector(state => state.auth)
+    const userId = auth.user.id
+    const { post, posts, loading } = useSelector((state) => state.posts);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const menu = Boolean(anchorEl);
+    const [openNote, setOpenNote] = useState(false);
+    const bookmarkedPosts = posts.filter(post => post.bookmarks == userId)
+    console.log(bookmarkedPosts)
+    const ClickMenu = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const CloseMenu = () => {
+      setAnchorEl(null);
+    };
+
+    useEffect(() => {
+        dispatch(getAllPosts())
+      })
+   
+
+
+    const renderPosts = bookmarkedPosts.map((post, index) => (
+      <div >
+        <Post
+            post ={post}      
+            show={show}
+        />
+    </div> 
+     
+    
+    ))
+
+    return (
+      <HomeContext.Provider value={{openNote, setOpenNote}}>
+        <Home>      
+        <Container className={classes.container}>
+        {loading ? 
+                 
+                 < CircularProgress size="3em" elevation={4} className={classes.loadingPaper} />
+                :
+                <>
+                
+           <Grid className={classes.mainContainer} container alignItems="stretch" spacing={2}> 
+          
+         {renderPosts}    
+         </Grid>
+         </>
+         }
+       
+        
+   
+         </Container>
+        </Home>
+        </HomeContext.Provider>
+    )
+}
+
+export default Bookmark
